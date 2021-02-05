7c31247ce8d7696d0e4864778fb49e1a14cb3663
Mohamed Hossain <mz.hossain212@gmail.com>
2021-02-05 09:42:31 -0500

    commit tree 9fd5ef472d4a6a4d4186d99fa3419ff22597ca3a

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+require 'pry'
+#     says that the shoe has been repaired (FAILED - 7)
+#     makes the shoe's condition new (FAILED - 8)
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+  
+    def initialize(brand)
+      @brand = brand
+    end
+  
+    def cobble
+      @condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+
